05f444ce8abe1155f5795188be8bcf8a5566c8fa
Tanya <kashyta90@gmail.com>
2020-08-13 09:47:01 -0700

    TFragments.05-Exercise-InterfaceCommunication
    Create OnImageClick Listener interface to trigger callbacks to host
    activity

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -17,14 +17,15 @@
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
-// TODO (4) Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  implements MasterListFragment.OnImageClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // TODO (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
+    @Override
+    public void OnImageSelected(int position) {
+        Toast.makeText(this,"Position Clicked =" + position,Toast.LENGTH_SHORT).show();
+    }
 
 }
